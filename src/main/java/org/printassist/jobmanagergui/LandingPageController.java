60f9edda874d7201221377dea43a9cdfb0d35f7f
Andreas Mayer <andreas.mayer@3bankenit.at>
2024-07-24 14:46:56 +0200

    -- implemented TabPane into landing-page-view.fxml -- implemented Jobs and Task content page in TabPane as Tabs. -- made changes to styling to background and color of text in Tabs

--- src/main/java/org/printassist/jobmanagergui/LandingPageController.java
+++ src/main/java/org/printassist/jobmanagergui/LandingPageController.java
@@ -1,7 +1,15 @@
 package org.printassist.jobmanagergui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.StackPane;
 
 public class LandingPageController {
-
-
+	@FXML
+	public Tab jobTab;
+	@FXML
+	public TabPane jobTableContentTabPane;
+	@FXML
+	public StackPane jobTabInclude;
 }
---
+++ src/main/java/org/printassist/jobmanagergui/TaskContent.java
@@ -0,0 +1,5 @@
+package org.printassist.jobmanagergui;
+
+public class TaskContent {
+
+}
